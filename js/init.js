5bef612a2a163c34a76f9d2e3e56505d68b3fcd5
Taigo Ito <webmaster@qwel.design>
2023-12-05 14:26:00 +0900

    Add scripts (make the menu mobile-friendly)

---
+++ js/init.js
@@ -0,0 +1,17 @@
+/**
+ * Init
+ * Author: Taigo Ito (https://qwel.design/)
+ * Location: Fukui, Japan
+ */
+
+// Drawer Menu
+import DrawerMenu from './_drawerMenu.js';
+new DrawerMenu();
+
+// Evil Icons
+import EvilIcons from './_evilIcons.js';
+new EvilIcons();
+
+// Slider
+import Slider from './_slider.js';
+new Slider();
